8f612f479d75b1d39a701e1e6ec295461852034e
小小小小鸡 <xiaojiowo>
2019-10-06 13:46:43 +0800

    由 Cloud Studio 更新，https://studio.dev.tencent.com

--- mysite/nlp/urls.py
+++ mysite/nlp/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.inputForm, name=''),  # 这里是默认首页
     # path('index/', views.index, name='index'),
     path('UploadFile/', views.UploadFile, name='UploadFile'),
-    path('UploadText/',views.UploadText,name='UploadText'),
+    path('UploadText/', views.UploadText, name='UploadText'),
     # path('inputForm/', views.inputForm, name='inputForm'),
 ]
